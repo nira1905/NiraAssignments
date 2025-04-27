12c728b71cb2a7b010201ac1d38da33cef775b6d
nira1905 <niranjanaranji@gmail.com>
2025-04-27 13:48:14 +0530

    Add files via upload
    Day1_26thApr

---
+++ day1/FirstProgram.java
@@ -0,0 +1,13 @@
+package week1.day1;
+
+import cucumber.api.cli.Main;
+
+public class FirstProgram {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("hellovj");
+		System.out.println("welcome to test leaf");
+	}
+
+}
---
+++ day1/LearnDataTypes.java
@@ -0,0 +1,28 @@
+package week1.day1;
+
+public class LearnDataTypes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    byte age=32;
+    System.out.println(age);
+    
+    int noOfLearners=100;
+    System.out.println(noOfLearners);
+    
+    long phNum = 98843108;
+    System.out.println(phNum);
+    
+    short regis=12345;
+    System.out.println(phNum);
+    
+    float speed=3.34567897f;
+    System.out.println(speed);
+    double speed1=3.3456789778;
+    System.out.println(speed1);
+    
+    
+	}
+  
+    
+}
